ccb8deccc20ad398d87b0e98160b851a9785925c
Chris Hansen <christopher.joe.hansen@gmail.com>
2020-06-14 15:57:47 -0700

    Updated Customer API to use AutoMapper

--- Vidly/Controllers/API/CustomersController.cs
+++ Vidly/Controllers/API/CustomersController.cs
@@ -5,53 +5,65 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Models;
+using AutoMapper;
+using Vidly.App_Start;
+using Vidly.Dtos;
 
 namespace Vidly.Controllers.API
 {
     public class CustomersController : ApiController
     {
         private ApplicationDbContext _context;
+        private Mapper _mapper;
 
         public CustomersController()
         {
             _context = new ApplicationDbContext();
+
+            var config = new MapperConfiguration(cnfg => cnfg.AddProfile<MappingProfile>());
+            _mapper = new Mapper(config);
         }
 
         // GET /api/customers
-        public IEnumerable<Customer> GetCustomers()
+        [HttpGet]
+        public IEnumerable<CustomerDto> GetCustomers()
         {
-            return _context.Customers.ToList();
+            return _context.Customers.ToList().Select(_mapper.Map<Customer, CustomerDto>);
         }
 
 
         // GET api/customers/1
-        public Customer GetCustomers(int id)
+        [HttpGet]
+        public CustomerDto GetCustomers(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
 
-            return customer;
+            return _mapper.Map<Customer, CustomerDto>(customer);
         }
 
         // POST /api/customers
         [HttpPost]
-        public Customer CreateCustomer(Customer customer)
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
 
+            var customer = _mapper.Map<CustomerDto, Customer>(customerDto);
             _context.Customers.Add(customer);
             _context.SaveChanges();
 
-            return customer;
+            customerDto.Id = customer.Id;
+
+            return customerDto;
         }
 
         // PUT /api/customers/1
         // id comes from the URL, customer comes from the body similar to CreateCustomer
         [HttpPut]
-        public void UpdateCustomer(int id, Customer customer)
+        public void UpdateCustomer(int id, CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
@@ -61,10 +73,7 @@ namespace Vidly.Controllers.API
             if (customerInDb == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
 
-            customerInDb.Name = customer.Name;
-            customerInDb.Birthdate = customer.Birthdate;
-            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
-            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            _mapper.Map(customerDto, customerInDb);
 
             _context.SaveChanges();
         }
--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -14,6 +14,7 @@ namespace Vidly
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
